c45a93ee79547d04c0d547d91126d653f54bf7ff
Liuddmyla <112398546+Liuddmyla@users.noreply.github.com>
2023-03-17 19:39:56 +0200

    2

--- src/components/statistics/Statistics.jsx
+++ src/components/statistics/Statistics.jsx
@@ -1,15 +1,20 @@
 import PropTypes from 'prop-types';
+import css from './Statistics.module.css';
+
+// function getRandomHexColor() {
+//     return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+// }
 
 export const Statistics = ({title, stats}) => {
     return (
-        <section className="statistics">
-            {title && (<h2 className="title">{title}</h2>)}
+        <section className={css.statistics}>
+            {title && (<h2 className={css.title}>{title}</h2>)}
             
-            <ul className="stat-list">
+            <ul className={css['stat-list']}>
                 {stats.map(e => (
-                    <li className="item" key={e.id}>
-                        <span className="label">{e.label}</span>
-                        <span className="percentage">{e.percentage}%</span>
+                    <li className={css.item} key={e.id}>
+                        <span className={css.label}>{e.label}</span>
+                        <span className={css.percentage}>{e.percentage}%</span>
                     </li>
                 ))}               
             </ul>
